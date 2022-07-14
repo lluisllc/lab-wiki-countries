bdaa8da5fe6b8c25d9995ff1704bb66b3ce5c04e
lluisllc <lluis.llacera@gmail.com>
2022-07-14 15:14:39 +0200

    Iteracion 1.2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 // src/App.js
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+  <Navbar />
+
+
+
+
+  </div>;
 }
 export default App;
